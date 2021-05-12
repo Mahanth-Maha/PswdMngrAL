514cbf60f0d51073c91f75564de340fbd927fffe
Mahanth <71886381+Mahanth-Maha@users.noreply.github.com>
2021-05-12 12:55:03 +0530

    Update OpenBrowser.py

--- Scraping/OpenBrowser.py
+++ Scraping/OpenBrowser.py
@@ -1,3 +1,17 @@
 import webbrowser
-AddressOfLoginPage = "https://login.salesforce.com/"
-webbrowser.get('windows-default').open(str(AddressOfLoginPage))
+import time
+
+def get_Address(addr):
+  if(addr != None):
+    AddressOfLoginPage = addr
+  else:
+    AddressOfLoginPage = "https://login.salesforce.com/
+  return AddressOfLoginPage
+    
+def open_browser(Address_Of_Login_Page):
+  webbrowser.get('windows-default').open(str(Address_Of_Login_Page))
+
+if __file__ == '__main__':
+  open_browser(get_Address(None))
+  time.sleep(1)
+  
